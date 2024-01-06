84037dfd2675c025214ef0c085b54a6673872bbb
alisher <alisher.201903953@gcuf.edu.pk>
2024-01-06 10:55:14 +0500

    code is  addded to url

--- resume/urls.py
+++ resume/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from newapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home,name="home"),
@@ -24,3 +26,5 @@ urlpatterns = [
     path("services/", include('services.urls')),
     path('skills/',include('skills.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
